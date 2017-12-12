9540a544f0cadeb8acae2966fdcd0a99a632b81a
gobi <gobi.subramani@gmail.com>
2017-12-12 09:23:59 +0530

    maze problem and solution

---
+++ src/com/general/RatMazePathCount.java
@@ -0,0 +1,16 @@
+package com.general;
+
+public class RatMazePathCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int maze[][] = {{1, 0, 0, 0},
+		         {1, 1, 0, 1},
+		         {0, 1, 0, 0},
+		         {1, 1, 1, 1}
+		     };
+
+	}
+
+}
